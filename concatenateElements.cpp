c61ab86399aa21e8bec5bbd29d0ad59235ab90de
Eddy Almanza <a01568010@gmail.com>
2021-03-26 19:13:28 -0500

    Add files via upload

---
+++ concatenateElements.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Concatenate {
+    public:
+        static string ConcatenateList(vector<string> list) {
+            string result = "";
+            for(int i = 0; i < list.size(); i++) {
+                result += list[i];
+                if(i == list.size() - 1) {
+                    break;
+                }
+                result += ",";
+            }
+
+            return result;
+        }
+};
+
+int main() {
+    vector<string> elements;
+    int sizeList = 0;
+    cout << "How many elements of the list? " << "\n";
+    cin >> sizeList;
+    cout << "Introduce the elements: " << "\n";
+    string listElements;
+    for(int i = 0; i < sizeList; i++) {
+        cin >> listElements;
+        elements.push_back(listElements);
+    }
+
+    Concatenate list;
+    cout << list.ConcatenateList(elements) << "\n";
+}
